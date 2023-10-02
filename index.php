e16e7c8ac39d17a827ad0b1f2b690f672e0b032d
LukaszMackowiak <102365309+LukaszMackowiak@users.noreply.github.com>
2023-10-02 11:49:03 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+$clientName = "Krzysztof";
+
+/* function showName () {
+    $clientName = "Adam";
+    return $clientName;
+} 
+
+ function addNumberToName ($clientName) {
+    $callBack = $clientName . "123";
+    return $callBack;
+} 
+
+function addNumberToName () {
+    global $clientName;
+    $clientName = $clientName . "123";
+    return $clientName;
+}
+
+echo addNumberToName($clientName);
+
+echo showName();
+echo $clientName; */
+
+?>
